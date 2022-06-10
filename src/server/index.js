155d8d66af4a0ccfe1e9f816756cbc099981d2a9
mira-kine <mirak0419@gmail.com>
2022-06-09 17:10:28 -0700

    working on launching to live server

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,8 @@
-import WebServer from "./web.server";
+import WebServer from './web.server';
 
-let webServer = new WebServer();
+const webServer = new WebServer();
 webServer.start().then(() => {
-    console.log('Web server started!')
+    console.log('Web server started!');
 }).catch(err => {
     console.error(err);
     console.error('Failed to start web server')
